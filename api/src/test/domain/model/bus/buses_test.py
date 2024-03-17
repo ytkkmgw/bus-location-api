f2e6d0091f58d8dca40d1d18caa3d2125869111b
ytkkmgw <ytkkmgw9914@gmail.com>
2024-03-17 23:29:29 +0900

    テストのクラス化

--- api/src/test/domain/model/bus/buses_test.py
+++ api/src/test/domain/model/bus/buses_test.py
@@ -1,4 +1,3 @@
-"""バス便一覧テストケース"""
 import pytest
 from api.src.test.domain.model.busstop.busstop_factory import BusstopFactory
 from api.src.test.domain.model.busstop.busstops_factory import BusstopsFactory
@@ -10,75 +9,76 @@ from domain.model.busstop.busstop import Busstop
 from domain.model.busstop.busstops import Busstops
 
 
-buses: Buses = Buses(
-    [
-        Bus(
-            BusIdentifier("odpt.Bus:SeibuBus.Yoshi60.52001.1.670"),
-            DepartureTime("17:30"),
-            BusstopFactory.create("東京女子大", 2),
-            BusstopFactory.create_empty(),
-        ),
-        Bus(
-            BusIdentifier("odpt.Bus:SeibuBus.Yoshi60.52001.1.670"),
-            DepartureTime("17:15"),
-            BusstopFactory.create("武蔵野第四小学校", 3),
-            BusstopFactory.create("立野町", 4),
-        ),
-        Bus(
-            BusIdentifier("odpt.Bus:SeibuBus.Yoshi60.52001.1.670"),
-            DepartureTime("17:00"),
-            BusstopFactory.create("関町南二丁目", 6),
-            BusstopFactory.create_empty(),
-        ),
-        Bus(
-            BusIdentifier("odpt.Bus:SeibuBus.Yoshi60.52001.1.670"),
-            DepartureTime("16:45"),
-            BusstopFactory.create("関町南二丁目", 6),
-            BusstopFactory.create("東京三育小学校入口", 7),
-        ),
-        Bus(
-            BusIdentifier("odpt.Bus:SeibuBus.Yoshi60.52001.1.669"),
-            DepartureTime("16:30"),
-            BusstopFactory.create("東京三育小学校入口", 7),
-            BusstopFactory.create_empty(),
-        ),
-    ]
-)
+class TestBuses:
+    """バス便一覧テストケース"""
 
+    buses: Buses = Buses(
+        [
+            Bus(
+                BusIdentifier("odpt.Bus:SeibuBus.Yoshi60.52001.1.670"),
+                DepartureTime("17:30"),
+                BusstopFactory.create("東京女子大", 2),
+                BusstopFactory.create_empty(),
+            ),
+            Bus(
+                BusIdentifier("odpt.Bus:SeibuBus.Yoshi60.52001.1.670"),
+                DepartureTime("17:15"),
+                BusstopFactory.create("武蔵野第四小学校", 3),
+                BusstopFactory.create("立野町", 4),
+            ),
+            Bus(
+                BusIdentifier("odpt.Bus:SeibuBus.Yoshi60.52001.1.670"),
+                DepartureTime("17:00"),
+                BusstopFactory.create("関町南二丁目", 6),
+                BusstopFactory.create_empty(),
+            ),
+            Bus(
+                BusIdentifier("odpt.Bus:SeibuBus.Yoshi60.52001.1.670"),
+                DepartureTime("16:45"),
+                BusstopFactory.create("関町南二丁目", 6),
+                BusstopFactory.create("東京三育小学校入口", 7),
+            ),
+            Bus(
+                BusIdentifier("odpt.Bus:SeibuBus.Yoshi60.52001.1.669"),
+                DepartureTime("16:30"),
+                BusstopFactory.create("東京三育小学校入口", 7),
+                BusstopFactory.create_empty(),
+            ),
+        ]
+    )
 
-@pytest.mark.parametrize(
-    "busstop_name, busstop_number, result_index",
-    [
-        ("東京女子大", 2, 0),
-        ("武蔵野第四小学校", 3, 0),
-        ("立野町", 4, 1),
-        ("関町南二丁目", 6, 2),
-        ("東京三育小学校入口", 7, 4),
-    ],
-)
-def test_recent_bus(busstop_name, busstop_number, result_index):
-    """直近のバス便を取得できる"""
+    @pytest.mark.parametrize(
+        "busstop_name, busstop_number, result_index",
+        [
+            ("東京女子大", 2, 0),
+            ("武蔵野第四小学校", 3, 0),
+            ("立野町", 4, 1),
+            ("関町南二丁目", 6, 2),
+            ("東京三育小学校入口", 7, 4),
+        ],
+    )
+    def test_recent_bus(self, busstop_name, busstop_number, result_index):
+        """直近のバス便を取得できる"""
 
-    base_busstop: Busstop = BusstopFactory.create(busstop_name, busstop_number)
-    busstops: Busstops = BusstopsFactory.create()
+        base_busstop: Busstop = BusstopFactory.create(busstop_name, busstop_number)
+        busstops: Busstops = BusstopsFactory.create()
 
-    actual = buses.recent_bus(base_busstop, busstops)
+        actual = self.buses.recent_bus(base_busstop, busstops)
 
-    assert buses.as_list()[result_index] == actual
+        assert self.buses.as_list()[result_index] == actual
 
+    @pytest.mark.parametrize(
+        "busstop_name, busstop_number",
+        [
+            ("吉祥寺駅", 0),
+            ("サンロード入口", 1),
+        ],
+    )
+    def test_no_recent_bus(self, busstop_name, busstop_number):
+        """直近のバス便が無い場合または始発バス停の場合はNoneが返る"""
+        base_busstop: Busstop = BusstopFactory.create(busstop_name, busstop_number)
+        busstops: Busstops = BusstopsFactory.create()
 
-@pytest.mark.parametrize(
-    "busstop_name, busstop_number",
-    [
-        ("吉祥寺駅", 0),
-        ("サンロード入口", 1),
-    ],
-)
-def test_no_recent_bus(busstop_name, busstop_number):
-    """直近のバス便が無い場合または始発バス停の場合はNoneが返る"""
-    base_busstop: Busstop = BusstopFactory.create(busstop_name, busstop_number)
-    busstops: Busstops = BusstopsFactory.create()
+        actual = self.buses.recent_bus(base_busstop, busstops)
 
-    actual = buses.recent_bus(base_busstop, busstops)
-
-    assert actual is None
+        assert actual is None
--- api/src/test/domain/model/bus/now_location_test.py
+++ api/src/test/domain/model/bus/now_location_test.py
@@ -1,4 +1,3 @@
-"""現在位置テストケース"""
 import pytest
 
 from api.src.test.domain.model.busstop.busstop_factory import BusstopFactory
@@ -11,42 +10,45 @@ from domain.model.bus.now_location import NowLocation
 from domain.model.busstop.busstop import Busstop
 from domain.model.busstop.busstops import Busstops
 
-buses: Buses = Buses(
-    [
-        Bus(
-            BusIdentifier("odpt.Bus:SeibuBus.Yoshi60.52001.1.670"),
-            DepartureTime("17:30"),
-            BusstopFactory.create("サンロード入口", 1),
-            BusstopFactory.create("東京女子大入口", 2),
-        ),
-        Bus(
-            BusIdentifier("odpt.Bus:SeibuBus.Yoshi60.52001.1.670"),
-            DepartureTime("17:00"),
-            BusstopFactory.create("武蔵野寮前", 5),
-            BusstopFactory.create_empty(),
-        ),
-    ]
-)
 
+class TestNowLocation:
+    """現在位置テストケース"""
 
-@pytest.mark.parametrize(
-    "busstop_name, busstop_number, excepted",
-    [
-        ("吉祥寺駅", 0, NowLocation.NONE),
-        ("サンロード入口", 1, NowLocation.NONE),
-        ("東京女子大入口", 2, NowLocation.SOON),
-        ("武蔵野第四小学校", 3, NowLocation.ONE_TWO),
-        ("立野町", 4, NowLocation.TWO_THREE),
-        ("武蔵野寮前", 5, NowLocation.NOW),
-        ("関町南二丁目", 6, NowLocation.ONE),
-        ("東京三育小学校入口", 7, NowLocation.TWO),
-    ],
-)
-def test_get_location(busstop_name, busstop_number, excepted):
-    """バス停一覧・バス便一覧から現在位置を取得できる"""
-    base_busstop: Busstop = BusstopFactory.create(busstop_name, busstop_number)
-    busstops: Busstops = BusstopsFactory.create()
+    buses: Buses = Buses(
+        [
+            Bus(
+                BusIdentifier("odpt.Bus:SeibuBus.Yoshi60.52001.1.670"),
+                DepartureTime("17:30"),
+                BusstopFactory.create("サンロード入口", 1),
+                BusstopFactory.create("東京女子大入口", 2),
+            ),
+            Bus(
+                BusIdentifier("odpt.Bus:SeibuBus.Yoshi60.52001.1.670"),
+                DepartureTime("17:00"),
+                BusstopFactory.create("武蔵野寮前", 5),
+                BusstopFactory.create_empty(),
+            ),
+        ]
+    )
 
-    actual = NowLocation.get_location(base_busstop, buses, busstops)
+    @pytest.mark.parametrize(
+        "busstop_name, busstop_number, excepted",
+        [
+            ("吉祥寺駅", 0, NowLocation.NONE),
+            ("サンロード入口", 1, NowLocation.NONE),
+            ("東京女子大入口", 2, NowLocation.SOON),
+            ("武蔵野第四小学校", 3, NowLocation.ONE_TWO),
+            ("立野町", 4, NowLocation.TWO_THREE),
+            ("武蔵野寮前", 5, NowLocation.NOW),
+            ("関町南二丁目", 6, NowLocation.ONE),
+            ("東京三育小学校入口", 7, NowLocation.TWO),
+        ],
+    )
+    def test_get_location(self, busstop_name, busstop_number, excepted):
+        """バス停一覧・バス便一覧から現在位置を取得できる"""
+        base_busstop: Busstop = BusstopFactory.create(busstop_name, busstop_number)
+        busstops: Busstops = BusstopsFactory.create()
 
-    assert actual == excepted
+        actual = NowLocation.get_location(base_busstop, self.buses, busstops)
+
+        assert actual == excepted
